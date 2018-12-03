90f303f33535b8e072ccb5bb7acaa9dec3d5b276
Hades\IXI <ixi150@gmail.com>
2018-12-03 18:56:08 +0100

    mastered water physics for the player and created demo scene

--- Assets/Game/Code/Player.cs
+++ Assets/Game/Code/Player.cs
@@ -11,45 +11,17 @@ namespace Game
 {
     public class Player : MonoBehaviour
     {
-        [SerializeField] new Vector2Reference constantForce;
-        [SerializeField] Vector2Reference jumpForce;
-        [SerializeField] ForceMode2D forceMode2D = ForceMode2D.Impulse;
-        [SerializeField] Vector2Reference maxSpeed;
         [SerializeField] Playable[] playablesOnJump;
         [SerializeField] FloatReference jumpCooldown;
         [SerializeField] float sceneReloadDelay = 3;
-
-        Rigidbody2D rb;
-
-        bool CanJump
-        {
-            get { return true; }
-        }
-
-        void Awake()
-        {
-            rb = GetComponent<Rigidbody2D>();
-        }
 
         void OnEnable()
         {
             StartCoroutine(InputCoorutine());
         }
 
-        void Update()
-        {
-
-        }
-
-        void FixedUpdate()
-        {
-            rb.AddForce((Vector2) constantForce * Time.deltaTime);
-            rb.velocity = rb.velocity.LimitedTo(maxSpeed);
-        }
-
         void Jump()
         {
-            rb.AddForce(jumpForce, forceMode2D);
             foreach (var playable in playablesOnJump)
                 playable.Play();
         }
@@ -84,7 +56,7 @@ namespace Game
         {
             while (true)
             {
-                if (ShouldJump() && CanJump)
+                if (ShouldJump())
                 {
                     Jump();
                     yield return new WaitForSeconds(jumpCooldown);
