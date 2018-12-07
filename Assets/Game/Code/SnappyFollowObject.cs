64d3959160819ae0aecde89fd186e26c672a2736
Hades\IXI <ixi150@gmail.com>
2018-12-07 22:09:23 +0100

    set up scrolling single background

---
+++ Assets/Game/Code/SnappyFollowObject.cs
@@ -0,0 +1,47 @@
+﻿using System.Linq;
+using UnityEngine;
+using Xunity.Behaviours;
+using Xunity.Extensions;
+using Xunity.ScriptableReferences;
+using Xunity.Sets;
+
+namespace Game
+{
+    public class SnappyFollowObject : GameBehaviour
+    {
+        [SerializeField] SetCollection targetReference;
+        [SerializeField] Vector3Reference snap;
+
+        Vector3 Snap
+        {
+            get { return snap; }
+        }
+
+        void LateUpdate()
+        {
+            var target = targetReference.Items.NotNull().FirstOrDefault();
+            if (target == null)
+                return;
+
+            var pos = target.position;
+            Position = new Vector3
+            (
+                SnapAxis(pos.x, 0),
+                SnapAxis(pos.y, 1),
+                SnapAxis(pos.z, 2)
+            );
+        }
+
+        float SnapAxis(float value, int axisIndex)
+        {
+            float s = Snap[axisIndex];
+            if (s == 0) 
+                return value;
+            if (s < 0)
+                return Position[axisIndex];
+
+            int count = Mathf.FloorToInt(value / s);
+            return count * s;
+        }
+    }
+}
